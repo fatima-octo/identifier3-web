a514bcf72ba99175e8ead3066ae7f50fb2b0fccd
fatima-octo <fatema@i4bahrain.com>
2026-06-03 13:02:03 +0300

    deploy: workstation skill-count + id-based assignments + attachment dedup
    Rebuild from identifier3 e47d452. Same base-href + SPA redirect; same URL.

--- main.dart.js_13.part.js
+++
@@ -1,6 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[24]
-var z=a.updateTypes([]);(function constants(){B.vw=new A.au(62328,"MaterialIcons",null,!1)})()};
-(a=>{a["WRV94dA6+3NRbe8sZ/Bo2FEp37Y="]=a.current})($__dart_deferred_initializers__);
